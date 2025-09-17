5aaccbc05771620034366740e25154cc27741751
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2025-09-17 18:17:35 +0300

    Update comments, edit variables names, created m_map().

--- src/labs/lab_1/src/Main.java
+++ src/labs/lab_1/src/Main.java
@@ -15,23 +15,35 @@ public class Main {
         for (int n = 19, i = 0; n >= 7; n -= 2, i++)
             w[i] = n;
         for (var i = 0; i < 10; i++)
-            x[i] = -11f + (5f + 11f) * random.nextFloat(); // random float in [-11;5]
+            x[i] = m_map(random.nextFloat(), 0f, 1f, -11f, 5f); // random float in [-11;5)
 
-        // Calculations and output
-        for (var i = 0; i < 7; i++) {
-            for (var j = 0; j < 10; j++) {
+        // Calculations
+        for (var i = 0; i < 7; i++)
+            for (var j = 0; j < 10; j++)
                 w_1[i][j] = m_calculate(x[j], w[i]);
-
-            }
-        }
-
+        
+        // Output
         m_printArray(w_1);
     }
 
     /**
-     * @param x  x[j]
-     * @param wi w[i]
-     * @return calculated value
+     * Maps a value from one range to another.
+     *
+     * @param value   the input value to be mapped.
+     * @param fromMin the lower bound of the input range.
+     * @param fromMax the upper bound of the input range.
+     * @param toMin   the lower bound of the target range.
+     * @param toMax   the upper bound of the target range.
+     * @return the mapped value in the target range.
+     */
+    private static float m_map(float value, float fromMin, float fromMax, float toMin, float toMax) {
+        return (value - fromMin) * (toMax - toMin) / (fromMax - fromMin) + toMin;
+    }
+
+    /**
+     * @param x  = x[j] (the input value from array x).
+     * @param wi = w[i] (the input value from array w).
+     * @return The calculated result as a float.
      */
     private static float m_calculate(final float x, final long wi) {
         return switch ((int) wi) {
@@ -42,12 +54,14 @@ public class Main {
     }
 
     /**
-     * @param arr 2-d array for printing.
+     * Prints a two-dimensional float array to the console in a formatted table.
+     *
+     * @param arr the 2D float array to print.
      */
     private static void m_printArray(final float[][] arr) {
-        for (var x : arr) {
-            for (var y : x) {
-                System.out.print(String.format(Locale.US, "%14.3f", y) + " ");
+        for (var line : arr) {
+            for (var value : line) {
+                System.out.print(String.format(Locale.US, "%14.3f", value) + " ");
             }
             System.out.println();
         }
