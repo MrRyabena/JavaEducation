4b390e98f849bc2a9595836a670e40b46ea9968b
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2025-10-03 00:00:46 +0300

    Testing pokemons.

---
+++ src/labs/lab_2/src/Main.java
@@ -0,0 +1,10 @@
+import ru.ifmo.se.pokemon.*;
+
+public class Main {
+    Battle b = new Battle();
+    Pokemon p1 = new Pokemon("Чужой", 1);
+    Pokemon p2 = new Pokemon("Хищник", 1);
+    // b.addAlly(p1);
+    // b.addFoe(p2);
+    b.go();
+}
