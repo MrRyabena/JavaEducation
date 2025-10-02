4f1e454808592b07d0f7708fd33826d2c4c99753
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-10-03 00:39:26 +0300

    Added PokemonSnower.

--- src/labs/lab_2/src/Main.java
+++ src/labs/lab_2/src/Main.java
@@ -1,10 +1,13 @@
 import ru.ifmo.se.pokemon.*;
-
 public class Main {
-    Battle b = new Battle();
-    Pokemon p1 = new Pokemon("Чужой", 1);
-    Pokemon p2 = new Pokemon("Хищник", 1);
-    // b.addAlly(p1);
-    // b.addFoe(p2);
-    b.go();
+    public static void main(String[] args) {
+        Battle b = new Battle();
+        Pokemon p1 = new Pokemon("Чужой", 1);
+        Pokemon p2 = new Pokemon("Хищник", 1);
+        Pokemon p3 = new PokemonSnover("Snover", 1);
+        b.addAlly(p1);
+        b.addFoe(p2);
+        b.addAlly(p3);
+        b.go();
+    }
 }
