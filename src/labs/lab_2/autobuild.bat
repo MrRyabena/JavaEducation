5982843ea10fe7ef49447d2adc34b5849b938d37
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-10-22 02:12:43 +0300

    Added compiling packages.

--- src/labs/lab_2/autobuild.bat
+++ src/labs/lab_2/autobuild.bat
@@ -1,12 +1,12 @@
 mkdir build
 
 cd src
-javac -d "../build/" -cp Pokemon.jar Main.java *.java
+javac -d "../build/" -cp Pokemon.jar Main.java Pokemons/*.java Moves/*.java
 copy Pokemon.jar ..\build\
 copy main.mf ..\build\
 
 cd ../build
-jar -cfm Lab_2.jar main.mf *.class
+jar -cfm Lab_2.jar main.mf *.class Pokemons/*.class Moves/*.class
 
 @REM java -cp .;Pokemon.jar Main
 
