ceb81aa9bda0911adcb877199b44951b673516e4
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-10-22 03:30:49 +0300

    Next version.

--- src/labs/lab_2/autobuild.sh
+++ src/labs/lab_2/autobuild.sh
@@ -9,11 +9,9 @@ cp main.mf ../build/
 cd ../build
 jar -cfm Lab_2.jar main.mf *.class Pokemons/*.class Moves/*.class
 
-ls -la
-# Проверяем, что JAR создался
+
 if [ -f "Lab_2.jar" ]; then
     echo "JAR file created successfully. Running..."
-    # Используем минимальные настройки JVM
     java -Xms64m -Xmx128m -XX:MaxMetaspaceSize=64m -jar Lab_2.jar
 else
     echo "Error: JAR file was not created!"
