800af95433f41f92f6e7bacd052bd092d5625b99
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-10-22 03:36:04 +0300

    Next attempt.

--- src/labs/lab_2/autobuild.sh
+++ src/labs/lab_2/autobuild.sh
@@ -1,9 +1,23 @@
 
+#!/bin/bash
+
+echo "=== Starting build process ==="
+echo "Java version:"
+java -version
+
+# Устанавливаем минимальные настройки JVM для всех команд
+export JAVA_OPTS="-Xms32m -Xmx64m -XX:MaxMetaspaceSize=32m"
+
 mkdir -p build
 
 cd src
 echo "Compiling Java files..."
-javac -d "../build/" -cp Pokemon.jar Main.java Pokemons/*.java Moves/*.java
+javac $JAVA_OPTS -d "../build/" -cp Pokemon.jar Main.java Pokemons/*.java Moves/*.java
+if [ $? -ne 0 ]; then
+    echo "Error: Compilation failed!"
+    exit 1
+fi
+
 echo "Copying files..."
 cp Pokemon.jar ../build/
 cp main.mf ../build/
@@ -11,11 +25,15 @@ cp main.mf ../build/
 cd ../build
 echo "Creating JAR file..."
 jar -cfm Lab_2.jar main.mf *.class Pokemons/*.class Moves/*.class
+if [ $? -ne 0 ]; then
+    echo "Error: JAR creation failed!"
+    exit 1
+fi
 
 echo "Running JAR file..."
 if [ -f "Lab_2.jar" ]; then
     echo "JAR file created successfully. Running..."
-    java -Xms64m -Xmx128m -XX:MaxMetaspaceSize=64m -jar Lab_2.jar
+    java $JAVA_OPTS -jar Lab_2.jar
 else
     echo "Error: JAR file was not created!"
     exit 1
