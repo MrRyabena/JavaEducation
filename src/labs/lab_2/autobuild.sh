4140e5109bfa19db8a86652d8458b1a409740f2b
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-10-22 03:33:12 +0300

    Added debug.

--- src/labs/lab_2/autobuild.sh
+++ src/labs/lab_2/autobuild.sh
@@ -2,14 +2,17 @@
 mkdir -p build
 
 cd src
+echo "Compiling Java files..."
 javac -d "../build/" -cp Pokemon.jar Main.java Pokemons/*.java Moves/*.java
+echo "Copying files..."
 cp Pokemon.jar ../build/
 cp main.mf ../build/
 
 cd ../build
+echo "Creating JAR file..."
 jar -cfm Lab_2.jar main.mf *.class Pokemons/*.class Moves/*.class
 
-
+echo "Running JAR file..."
 if [ -f "Lab_2.jar" ]; then
     echo "JAR file created successfully. Running..."
     java -Xms64m -Xmx128m -XX:MaxMetaspaceSize=64m -jar Lab_2.jar
