42871779f40910a993b9f62006e1748df0a8dbd6
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-10-22 05:28:20 +0300

    Completed.

--- src/labs/lab_2/autobuild.sh
+++ src/labs/lab_2/autobuild.sh
@@ -2,11 +2,11 @@
 mkdir -p build
 
 cd src
-javac -Xmx512m -XX:MaxMetaspaceSize=128m -d "../build/" -cp Pokemon.jar Main.java Pokemons/*.java Moves/*.java
+javac -d "../build/" -cp Pokemon.jar Main.java Pokemons/*.java Moves/*.java
 cp Pokemon.jar ../build/
 cp main.mf ../build/
 
 cd ../build
-jar -Xmx512m -XX:MaxMetaspaceSize=128m -cfm Lab_2.jar main.mf *.class Pokemons/*.class Moves/*.class
+jar -cfm Lab_2.jar main.mf *.class Pokemons/*.class Moves/*.class
 
 java -Xmx512m -XX:MaxMetaspaceSize=128m -jar Lab_2.jar
