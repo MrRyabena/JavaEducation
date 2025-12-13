1738569cd9687194776425ccd774319e41d87dc6
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2025-12-14 02:37:43 +0300

    Developing script.

--- src/labs/lab_3-4/src/Main.java
+++ src/labs/lab_3-4/src/Main.java
@@ -1,9 +1,37 @@
+import characters.*;
+
 import transport.*;
 
 public class Main {
     public static void main(String[] args) {
-        var car = new Automobile("My car");
+        try {
 
-        System.out.println(car.toString());
+            var Dunno = new Dunno();
+            Dunno.onStatusChange(
+                    (obj) -> {
+                        System.out.println(obj.toString());
+                    });
+            var Smarty = new Smarty();
+            Smarty.onStatusChange(
+                    (obj) -> {
+                        System.out.println(obj.toString());
+                    });
+
+            Dunno.setWish(Wish.TRAVEL);
+
+            var balloon = Smarty.makeBalloon();
+            balloon.onStatusChange(
+                    (obj) -> {
+                        System.out.println(obj.toString());
+                    });
+            Smarty.boardTransport(balloon);
+            Dunno.boardTransport(balloon);
+
+            balloon.start();
+
+        } catch (core.ScriptException e) {
+            System.err.println(e.toString());
+            return;
+        }
     }
 }
