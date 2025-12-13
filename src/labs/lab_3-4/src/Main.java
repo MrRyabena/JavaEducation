bd10156e416f88cab17b029a5485800feb825191
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2025-12-13 15:18:29 +0300

    Created main class.

---
+++ src/labs/lab_3-4/src/Main.java
@@ -0,0 +1,9 @@
+import transport.*;
+
+public class Main {
+    public static void main(String[] args) {
+        var car = new Automobile("My car");
+
+        System.out.println(car.toString());
+    }
+}
