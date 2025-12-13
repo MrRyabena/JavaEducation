7705de77b0d1e45e905c82b6c4b0804831254a91
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2025-12-14 02:38:24 +0300

    Added packege characters.

--- src/labs/lab_3-4/autobuild.sh
+++ src/labs/lab_3-4/autobuild.sh
@@ -1,10 +1,10 @@
 mkdir -p build
 
 cd src
-javac -d "../build/" Main.java transport/*.java
+javac -d "../build/" Main.java characters/*.java transport/*.java
 cp main.mf ../build/
 
 cd ../build
-jar -cfm lab_3-4.jar main.mf Main.class transport/*.class
+jar -cfm lab_3-4.jar main.mf Main.class core/*.class characters/*.class transport/*.class
 
 java -Xmx512m -XX:MaxMetaspaceSize=128m -jar lab_3-4.jar
