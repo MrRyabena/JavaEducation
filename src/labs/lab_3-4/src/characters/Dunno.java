97fcc0dbbc506035c7afae49cc3e38c19e7db694
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2025-12-15 11:13:39 +0300

    Multiple creation of an object is prohibited.

--- src/labs/lab_3-4/src/characters/Dunno.java
+++ src/labs/lab_3-4/src/characters/Dunno.java
@@ -1,9 +1,12 @@
 package characters;
 
-import transport.Balloon;
-
-public class Dunno extends Character {
-    public Dunno() {
+public final class Dunno extends Character {
+    public Dunno() throws core.ScriptException {
         super("Dunno");
+
+        if (m_created) throw new core.ScriptException("Only one instance of Dunno is allowed!");
+        m_created = true;
     }
+
+    private static boolean m_created = false;
 }
