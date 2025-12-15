a0ed9948353a9cd06048f656b79c184e775b49ba
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2025-12-15 11:17:30 +0300

    Supplemented the script.

--- src/labs/lab_3-4/src/Main.java
+++ src/labs/lab_3-4/src/Main.java
@@ -1,37 +1,60 @@
 import characters.*;
-
 import transport.*;
 
 public class Main {
     public static void main(String[] args) {
         try {
+            java.util.function.Consumer<core.Statusable> callback =
+                    (obj) -> {
+                        System.out.println(obj.toString());
+
+                        try {
+                            Thread.sleep(core.Actionable.delay_timeout);
+                        } catch (InterruptedException e) {
+                            System.err.println("Script interrupted: " + e.toString());
+                        }
+                    };
 
             var Dunno = new Dunno();
-            Dunno.onStatusChange(
-                    (obj) -> {
-                        System.out.println(obj.toString());
-                    });
+            Dunno.onStatusChange(callback);
             var Smarty = new Smarty();
-            Smarty.onStatusChange(
-                    (obj) -> {
-                        System.out.println(obj.toString());
-                    });
+            Smarty.onStatusChange(callback);
 
             Dunno.setWish(Wish.TRAVEL);
 
-            var balloon = Smarty.makeBalloon();
-            balloon.onStatusChange(
-                    (obj) -> {
-                        System.out.println(obj.toString());
-                    });
-            Smarty.boardTransport(balloon);
+            var balloon = Smarty.inventTransport(TransportType.BALLOON);
+            balloon.onStatusChange(callback);
+
+            Dunno.see(balloon);
+            Dunno.setWish(Wish.FLY);
             Dunno.boardTransport(balloon);
 
-            balloon.start();
+            Smarty.setFeeling(Feeling.FEAR);
+            Smarty.boardTransport(balloon);
+
+            try {
+                balloon.start();
+                balloon.action();
+                Smarty.setFeeling(Feeling.HAPPY);
+                Dunno.setFeeling(Feeling.FEAR);
+                balloon.action();
+                balloon.stop();
+            } catch (BreakException e) {
+                e.toString();
+            }
+
+            Dunno.leaveTransport(balloon);
+            Smarty.leaveTransport(balloon);
+            
+            Smarty.setWish(Wish.INVENT);
+            Smarty.setFeeling(Feeling.HAPPY);
+
+
 
         } catch (core.ScriptException e) {
             System.err.println(e.toString());
-            return;
+        } catch (InterruptedException e) {
+            System.err.println("Script interrupted: " + e.toString());
         }
     }
 }
