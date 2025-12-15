e3588adde00b3e433fa080c18752cf960971f400
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2025-12-15 15:52:52 +0300

    Created abstract class Engine.

---
+++ src/labs/lab_3-4/src/transport/Engine.java
@@ -0,0 +1,38 @@
+package transport;
+
+public abstract class Engine extends core.Statusable {
+    public static enum Fuel {
+        WATER,
+        CARBONATED_WATER,
+        GAS
+    }
+
+    public static record Parameters(int power, Fuel fuel_type) {}
+
+    public final EngineTypes type;
+    public final Parameters parameters;
+    protected boolean m_state;
+
+    public Engine(EngineTypes set_type, Parameters params) {
+        type = set_type;
+        parameters = params;
+    }
+
+    public void start() {
+        m_state = true;
+        m_setStatus("start");
+    }
+
+    public void stop() {
+        m_state = false;
+        m_setStatus("stop");
+    }
+
+    public boolean isWorking() {
+        return m_state;
+    }
+
+    public String toString() {
+        return type.toString() + m_status;
+    }
+}
