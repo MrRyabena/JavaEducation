b2ec36f89b84b73289bf2d7dd1cc2db0fb8485ea
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2026-02-17 00:25:55 +0300

    Created wrapper class Collection.

---
+++ src/labs/lab_5/src/collection/Collection.java
@@ -0,0 +1,71 @@
+package collection;
+
+import java.util.HashSet;
+import dragon.Dragon;
+import java.util.function.Predicate;
+
+public class Collection implements Interaction {
+    public Collection() {
+        this.buffer = new HashSet<>();
+    }
+
+    @Override
+    public void add(Dragon element) {
+        buffer.add(element);
+    }
+
+    @Override
+    public void updateId(long id, Dragon element) {
+        buffer.removeIf(e -> e.getId() == id);
+        if (element != null)
+            buffer.add(element);
+    }
+
+    @Override
+    public void removeById(long id) {
+        buffer.removeIf(e -> e.getId() == id);
+    }
+
+    @Override
+    public void removeGreater(Dragon lower) {
+        buffer.removeIf(e -> e.compareTo(lower) > 0);
+    }
+
+    @Override
+    public void clear() {
+        buffer.clear();
+    }
+
+    @Override
+    public int countByType(dragon.DragonType type) {
+        return countIf(e -> e.getType() == type);
+    }
+
+    @Override
+    public int countGreaterThanType(dragon.DragonType type) {
+        return countIf(e -> e.getType().compareTo(type) > 0);
+    }
+
+    private int countIf(Predicate<? super Dragon> filter) {
+        int counter = 0;
+
+        for (var x : buffer)
+            if (filter.test(x))
+                counter++;
+
+        return counter;
+    }
+
+    @Override
+    public HashSet<Dragon> filterStartsWithName(String sub_name) {
+        var output = new HashSet<Dragon>();
+        for (var x : buffer)
+            if (x.getName().startsWith(sub_name))
+                output.add(x);
+
+        return output;
+    }
+
+    private HashSet<Dragon> buffer;
+
+}
