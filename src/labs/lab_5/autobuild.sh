425d60c47670a424f7e185ddd8c358796d3692b9
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2026-02-17 00:31:40 +0300

    Added collection to build.

--- src/labs/lab_5/autobuild.sh
+++ src/labs/lab_5/autobuild.sh
@@ -1,10 +1,12 @@
 mkdir -p build
 
+mvn clean install
+
 cd src
-javac -d "../build/" Main.java core/*.java dragon/*.java
+javac -d "../build/" Main.java core/*.java dragon/*.java collection/*.java
 cp main.mf ../build/
 
 cd ../build
-jar -cfm lab_5.jar main.mf Main.class core/*.class dragon/*.class
+jar -cfm lab_5.jar main.mf Main.class core/*.class dragon/*.class collection/*.class
 
 java -Xmx512m -XX:MaxMetaspaceSize=128m -jar lab_5.jar
